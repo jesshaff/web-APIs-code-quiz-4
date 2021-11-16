7ea5b6812cf270e2d59aaeb09cb65caf564c3720
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2021-11-15 20:00:07 -0500

    initial setup of html and css

---
+++ Develop/script.js
@@ -0,0 +1,11 @@
+// Start Button
+
+// Timer starts - presented with 1st question
+
+// Question is answered - presented with 2n3 question
+
+// Answer incorrectly time is subtracted from clock
+
+// All questions answered or timer reaches zero then the game is over
+
+// Save my initials and score
