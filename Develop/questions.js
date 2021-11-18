e599b8a39d8d33ca721347da3589bd9f68169e31
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2021-11-18 09:34:40 -0500

    updates to index and creation of questions.js and score.js - wip

---
+++ Develop/questions.js
@@ -0,0 +1,52 @@
+// list of questions, choices and answers for quiz
+
+var questions {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: [
+        "<js>", 
+        "<scripting>", 
+        "<script>", 
+        "<javascript>"
+    ],
+    answer: "script"
+},
+{
+    question: "Commonly used data types DO NOT include:",
+    choices: [
+        "strings",
+        "booleans",
+        "alerts",
+        "numbers"
+    ],
+    answer: "alerts"
+},
+{
+    question: "What does HTML stand for?"
+    choices: [
+        "Hyperlinks and Text Markup Language",
+        "Hypertext Markup Language",
+        "Home Tool Markup Language",
+        "Hypertext Markdown Language"
+    ],
+    answer: "Hypertext Markup Language"
+},
+{
+    question: "What does CSS stand for?"
+    choices: [
+        "Colorful Style Sheets",
+        "Creative Style Sheets",
+        "Computer Style Sheets",
+        "Cascading Style Sheets"
+],
+    answer: "Cascading Style Sheets"
+},
+{
+    question: "Arrays in JavaScript can be used to store _____."
+    choices: [
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"
+    ],
+    answer: "all of the above"
+};
---
+++ Develop/score.js
@@ -0,0 +1,16 @@
+function displayHighscores() {
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    // sort highscores in descending order
+    highscores.sort(function(a,b) {
+        return b.score - a.score;
+    });
+
+    highscores.forEach(function(score) {
+        var liTag = document.createElement("li");
+        liTag.textContent = score.initials + " - " + score.score;
+
+        // display 
+        var olEL = document.getElementById("highscores");
+        olEL.appendChild(liTag);
+    })
